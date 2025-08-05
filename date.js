fb0623a92be5295641db389e61261f0b2378a788
sourav-technizoit <sourav@technizoit.com>
2025-08-05 21:20:32 +0530

    changes

--- date.js
+++ date.js
@@ -42,3 +42,53 @@ console.log("hours", mins)
 
 const sec = customDate.getSeconds()
 console.log("hours", sec)
+
+console.log(date)
+//convert the whole date into string
+//transfering the date formet to String formet
+const ConvertDateToString = date.toString()
+console.log("ConvertDateToString", ConvertDateToString)
+
+//take only the Day from date
+const ConvertDateToDateString = date.toDateString()
+console.log("ConvertDateToDateString", ConvertDateToDateString)
+
+//take only the time from date
+const ConvertDateToTimeString = date.toTimeString()
+console.log("ConvertDateToTimeString", ConvertDateToTimeString)
+
+//covert the date into MongoDB understable formet
+const ConvertDateToISOString = date.toISOString()
+console.log("ConvertDateToISOString", ConvertDateToISOString)
+
+// to understand the date into your own formet
+const ConvertDateToLocalString = date.toLocaleString()
+console.log("ConvertDateToLocalString", ConvertDateToLocalString)
+
+// usage of operators inside dates
+
+const d1 = new Date("2025-03-21");
+console.log(d1)
+console.log(d1.getTime())
+const d2 = new Date("2025-10-31");
+console.log(d2)
+console.log(d2.getTime())
+
+const isTwoDatesAreSame = d1.getTime() === d2.getTime();
+console.log("isTwoDatesAreSame", isTwoDatesAreSame)
+const isD1GreaterThanD2 = d1 > d2
+console.log("isD1GreaterThanD2", isD1GreaterThanD2)
+const isD2GreaterThanD1 = d2 > d1
+console.log("isD2GreaterThanD1", isD2GreaterThanD1)
+const differentinMillisec = d2 - d1
+console.log("differentinMillisec", differentinMillisec)
+
+
+// difference in between two dates
+// 1min=1000ms
+// 60sec=1min
+// 60min=1hour
+// 1day=24hours
+const convertThisMilliSecIntoDays = differentinMillisec / (1000 * 60 * 60 * 24)
+console.log("convertThisMilliSecIntoDays", convertThisMilliSecIntoDays)
+
